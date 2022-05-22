05247f74662d961a324b475e4e0a31678ffa19b3
Yuya Takashina <takashina2051@gmail.com>
2022-05-23 08:51:10 +0900

    Initial commit

---
+++ ci_template/constants.py
@@ -0,0 +1,8 @@
+"""Constants commonly used in the project.
+
+.. warning:: NEVER change the value of constants AT RUN TIME.
+"""
+
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).parent.parent
